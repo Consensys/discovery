154f50a370f99e1d96a514844eac61a639963638
Shahan Khatchadourian <shahan.k.code@gmail.com>
2019-12-05 15:19:50 -0500

    Small fix to test script
    Script now sets up and removes docker active containers and network bridge

--- src/test/resources/interop/run-by-screen.sh
+++ src/test/resources/interop/run-by-screen.sh
@@ -10,10 +10,22 @@ SCRIPT_DIR=`pwd`
 # Create the arrays to hold the input files that were specified
 INPUTS=()
 
-docker rm -f $(docker container ls -a -q)
+## WARNING: this script torches any active containers, and networks labeled mynet123
 
+echo "Test starting..."
+echo "Removing any active docker containers, and the network with label 'mynet123'"
+docker rm -f $(docker container ls -a -q)
+docker network rm mynet123
+
+echo "Creating docker network bridge 'mynet123'"
+docker network create --subnet=172.18.0.0/16 mynet123
+
+echo "Starting docker containers"
 screen -c interop.screenrc
 
+echo "Removing any active docker containers, and the network with label 'mynet123'"
 docker rm -f $(docker container ls -a -q)
+docker network rm mynet123
+echo "Test finished..."
 
 #set +o xtrace
