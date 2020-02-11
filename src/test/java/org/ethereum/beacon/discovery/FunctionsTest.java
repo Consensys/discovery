45266b6bad15cda1beab096e41e9606ac0a9b6cd
Adrian Sutton <adrian.sutton@consensys.net>
2020-02-12 08:47:50 +1000

    Add test vectors from the spec.

--- src/test/java/org/ethereum/beacon/discovery/FunctionsTest.java
+++ src/test/java/org/ethereum/beacon/discovery/FunctionsTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.apache.tuweni.bytes.Bytes;
 import org.ethereum.beacon.discovery.packet.AuthHeaderMessagePacket;
 import org.ethereum.beacon.discovery.util.Functions;
+import org.ethereum.beacon.discovery.util.Functions.HKDFKeys;
 import org.ethereum.beacon.discovery.util.Utils;
 import org.junit.jupiter.api.Test;
 import org.web3j.crypto.ECKeyPair;
@@ -77,6 +78,39 @@ public class FunctionsTest {
             Bytes.wrap(ECKeyPair.create(testKey1.toArray()).getPublicKey().toByteArray()),
             idNonce);
     assertEquals(keys1, keys2);
+  }
+
+  @Test
+  public void specTestVector_ecdh() {
+    final Bytes publicKey =
+        Bytes.fromHexString(
+            "0x9961e4c2356d61bedb83052c115d311acb3a96f5777296dcf297351130266231503061ac4aaee666073d7e5bc2c80c3f5c5b500c1cb5fd0a76abbb6b675ad157");
+    final Bytes secretKey =
+        Bytes.fromHexString("0xfb757dc581730490a1d7a00deea65e9b1936924caaea8f44d476014856b68736");
+    final Bytes result = Functions.deriveECDHKeyAgreement(secretKey, publicKey);
+    assertEquals(
+        Bytes.fromHexString("0x033b11a2a1f214567e1537ce5e509ffd9b21373247f2a3ff6841f4976f53165e7e"),
+        result);
+  }
+
+  @Test
+  public void specTestVector_keyDerivation() {
+    final Bytes secretKey =
+        Bytes.fromHexString("0x02a77e3aa0c144ae7c0a3af73692b7d6e5b7a2fdc0eda16e8d5e6cb0d08e88dd04");
+    final Bytes nodeIdA =
+        Bytes.fromHexString("0xa448f24c6d18e575453db13171562b71999873db5b286df957af199ec94617f7");
+    final Bytes nodeIdB =
+        Bytes.fromHexString("0x885bba8dfeddd49855459df852ad5b63d13a3fae593f3f9fa7e317fd43651409");
+    final Bytes idNonce =
+        Bytes.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+
+    final HKDFKeys hkdfKeys = Functions.hkdf_expand(nodeIdA, nodeIdB, secretKey, idNonce);
+    assertEquals(
+        Bytes.fromHexString("0x238d8b50e4363cf603a48c6cc3542967"), hkdfKeys.getInitiatorKey());
+    assertEquals(
+        Bytes.fromHexString("0xbebc0183484f7e7ca2ac32e3d72c8891"), hkdfKeys.getRecipientKey());
+    assertEquals(
+        Bytes.fromHexString("0xe987ad9e414d5b4f9bfe4ff1e52f2fae"), hkdfKeys.getAuthResponseKey());
   }
 
   @Test
