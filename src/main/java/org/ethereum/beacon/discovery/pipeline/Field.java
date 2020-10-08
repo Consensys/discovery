e3bd4e6a532092d7af9dffcd653174ccb9bca44f
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-10-08 15:44:00 +0300

    Sptoless apply

--- src/main/java/org/ethereum/beacon/discovery/pipeline/Field.java
+++ src/main/java/org/ethereum/beacon/discovery/pipeline/Field.java
@@ -21,14 +21,10 @@ public class Field<T> {
 
   public static final Field<SessionLookup> SESSION_LOOKUP =
       new Field<>("SESSION_LOOKUP"); // Node id, requests session lookup
-  public static final Field<NodeSession> SESSION =
-      new Field<>("SESSION"); // Node session
-  public static final Field<Request<?>> REQUEST =
-      new Field<>("REQUEST"); // Task to perform
-  public static final Field<Object> INCOMING =
-      new Field<>("INCOMING"); // Raw incoming data
-  public static final Field<Packet<?>> PACKET =
-      new Field<>("PACKET"); // unknown inbound packet
+  public static final Field<NodeSession> SESSION = new Field<>("SESSION"); // Node session
+  public static final Field<Request<?>> REQUEST = new Field<>("REQUEST"); // Task to perform
+  public static final Field<Object> INCOMING = new Field<>("INCOMING"); // Raw incoming data
+  public static final Field<Packet<?>> PACKET = new Field<>("PACKET"); // unknown inbound packet
   public static final Field<WhoAreYouPacket> PACKET_WHOAREYOU =
       new Field<>("PACKET_WHOAREYOU"); // WhoAreYou packet
   public static final Field<HandshakeMessagePacket> PACKET_HANDSHAKE =
@@ -43,10 +39,8 @@ public class Field<T> {
       new Field<>("MASKING_IV"); // need to keep RawPacket IV for decrypting a message
   public static final Field<InetSocketAddress> REMOTE_SENDER =
       new Field<>("REMOTE_SENDER"); // InetSocketAddress of remote sender
-  public static final Field<NodeRecord> NODE =
-      new Field<>("NODE"); // Sender/recipient node
-  public static final Field<Object> BAD_PACKET =
-      new Field<>("BAD_PACKET"); // Bad, rejected packet
+  public static final Field<NodeRecord> NODE = new Field<>("NODE"); // Sender/recipient node
+  public static final Field<Object> BAD_PACKET = new Field<>("BAD_PACKET"); // Bad, rejected packet
   public static final Field<Throwable> BAD_EXCEPTION =
       new Field<>("BAD_EXCEPTION"); // Stores exception for bad packet or message
 
