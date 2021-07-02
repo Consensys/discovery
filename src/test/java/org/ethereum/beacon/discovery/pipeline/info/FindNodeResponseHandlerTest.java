5c1f4d0f6ace57a224acf60d3d0b05cbc0022964
Adrian Sutton <adrian.sutton@consensys.net>
2021-07-02 11:32:54 +1000

    Move tests over to response handler.

--- src/test/java/org/ethereum/beacon/discovery/pipeline/info/FindNodeResponseHandlerTest.java
+++ src/test/java/org/ethereum/beacon/discovery/pipeline/info/FindNodeResponseHandlerTest.java
@@ -1,10 +1,10 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  */
-
-package org.ethereum.beacon.discovery.message.handler;
+package org.ethereum.beacon.discovery.pipeline.info;
 
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -14,15 +14,10 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 import org.apache.tuweni.bytes.Bytes;
 import org.ethereum.beacon.discovery.TestUtil;
 import org.ethereum.beacon.discovery.TestUtil.NodeInfo;
-import org.ethereum.beacon.discovery.message.FindNodeMessage;
 import org.ethereum.beacon.discovery.message.NodesMessage;
-import org.ethereum.beacon.discovery.pipeline.info.FindNodeResponseHandler;
-import org.ethereum.beacon.discovery.pipeline.info.Request;
-import org.ethereum.beacon.discovery.pipeline.info.RequestInfo;
 import org.ethereum.beacon.discovery.schema.NodeRecord;
 import org.ethereum.beacon.discovery.schema.NodeRecordInfo;
 import org.ethereum.beacon.discovery.schema.NodeSession;
@@ -31,13 +26,11 @@ import org.ethereum.beacon.discovery.util.Functions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class NodesHandlerTest {
-
+class FindNodeResponseHandlerTest {
   private static final Bytes PEER_ID = Bytes.fromHexString("0x1234567890ABCDEF");
   private static final Bytes REQUEST_ID = Bytes.fromHexString("0x1234");
   private final NodeSession session = mock(NodeSession.class);
   private final NodeTable nodeTable = mock(NodeTable.class);
-  private final NodesHandler handler = new NodesHandler();
 
   @BeforeEach
   public void setUp() {
@@ -49,21 +42,15 @@ class NodesHandlerTest {
   public void shouldAddReceivedRecordsToNodeTableButNotNodeBuckets() {
     final NodeInfo nodeInfo = TestUtil.generateNode(9000);
     final int distance = Functions.logDistance(PEER_ID, nodeInfo.getNodeRecord().getNodeId());
-    Request<Void> request =
-        new Request<>(
-            new CompletableFuture<>(),
-            id -> new FindNodeMessage(id, singletonList(distance)),
-            new FindNodeResponseHandler(singletonList(distance)));
-    final RequestInfo requestInfo = RequestInfo.create(REQUEST_ID, request);
-    when(session.getRequestInfo(REQUEST_ID)).thenReturn(Optional.of(requestInfo));
+    final FindNodeResponseHandler handler = new FindNodeResponseHandler(singletonList(distance));
+
     final List<NodeRecord> records = singletonList(nodeInfo.getNodeRecord());
     final NodesMessage message = new NodesMessage(REQUEST_ID, records.size(), records);
-    handler.handle(message, session);
+    assertThat(handler.handleResponseMessage(message, session)).isTrue();
 
     final NodeRecordInfo nodeRecordInfo = NodeRecordInfo.createDefault(nodeInfo.getNodeRecord());
     verify(nodeTable).save(nodeRecordInfo);
     verify(session, never()).putRecordInBucket(nodeRecordInfo);
-    verify(session).clearRequestInfo(REQUEST_ID, null);
   }
 
   @Test
@@ -73,26 +60,19 @@ class NodesHandlerTest {
     when(nodeTable.getNode(originalRecord.getNodeId()))
         .thenReturn(Optional.of(NodeRecordInfo.createDefault(originalRecord)));
     final int distance = Functions.logDistance(PEER_ID, originalRecord.getNodeId());
-    Request<Void> request =
-        new Request<>(
-            new CompletableFuture<>(),
-            id -> new FindNodeMessage(id, singletonList(distance)),
-            new FindNodeResponseHandler(singletonList(distance)));
-    final RequestInfo requestInfo = RequestInfo.create(REQUEST_ID, request);
-    when(session.getRequestInfo(REQUEST_ID)).thenReturn(Optional.of(requestInfo));
+    final FindNodeResponseHandler handler = new FindNodeResponseHandler(singletonList(distance));
 
     final NodeRecord updatedRecord =
         originalRecord.withUpdatedCustomField(
             "test", Bytes.fromHexString("0x8888"), nodeInfo.getPrivateKey());
     final List<NodeRecord> records = singletonList(updatedRecord);
     final NodesMessage message = new NodesMessage(REQUEST_ID, records.size(), records);
-    handler.handle(message, session);
+    assertThat(handler.handleResponseMessage(message, session)).isTrue();
 
     final NodeRecordInfo nodeRecordInfo = NodeRecordInfo.createDefault(updatedRecord);
     verify(nodeTable).save(nodeRecordInfo);
     verify(session, never()).updateNodeRecord(any());
     verify(session, never()).putRecordInBucket(any());
-    verify(session).clearRequestInfo(REQUEST_ID, null);
   }
 
   @Test
@@ -101,13 +81,7 @@ class NodesHandlerTest {
     final NodeRecord originalRecord = nodeInfo.getNodeRecord();
     when(nodeTable.getNode(originalRecord.getNodeId()))
         .thenReturn(Optional.of(NodeRecordInfo.createDefault(originalRecord)));
-    Request<Void> request =
-        new Request<>(
-            new CompletableFuture<>(),
-            id -> new FindNodeMessage(id, singletonList(0)),
-            new FindNodeResponseHandler(singletonList(0)));
-    final RequestInfo requestInfo = RequestInfo.create(REQUEST_ID, request);
-    when(session.getRequestInfo(REQUEST_ID)).thenReturn(Optional.of(requestInfo));
+    final FindNodeResponseHandler handler = new FindNodeResponseHandler(singletonList(0));
     // Requesting from the node itself
     when(session.getNodeId()).thenReturn(originalRecord.getNodeId());
 
@@ -116,29 +90,22 @@ class NodesHandlerTest {
             "test", Bytes.fromHexString("0x8888"), nodeInfo.getPrivateKey());
     final List<NodeRecord> records = singletonList(updatedRecord);
     final NodesMessage message = new NodesMessage(REQUEST_ID, records.size(), records);
-    handler.handle(message, session);
+    handler.handleResponseMessage(message, session);
 
     final NodeRecordInfo nodeRecordInfo = NodeRecordInfo.createDefault(updatedRecord);
     verify(nodeTable).save(nodeRecordInfo);
     verify(session).updateNodeRecord(updatedRecord);
     verify(session, never()).putRecordInBucket(any());
-    verify(session).clearRequestInfo(REQUEST_ID, null);
   }
 
   @Test
   public void shouldRejectReceivedRecordsThatAreInvalid() {
     final NodeInfo nodeInfo = TestUtil.generateInvalidNode(9000);
     final int distance = Functions.logDistance(PEER_ID, nodeInfo.getNodeRecord().getNodeId());
-    Request<Void> request =
-        new Request<>(
-            new CompletableFuture<>(),
-            id -> new FindNodeMessage(id, singletonList(distance)),
-            new FindNodeResponseHandler(singletonList(distance)));
-    final RequestInfo requestInfo = RequestInfo.create(REQUEST_ID, request);
-    when(session.getRequestInfo(REQUEST_ID)).thenReturn(Optional.of(requestInfo));
+    final FindNodeResponseHandler handler = new FindNodeResponseHandler(singletonList(distance));
     final List<NodeRecord> records = singletonList(nodeInfo.getNodeRecord());
     final NodesMessage message = new NodesMessage(REQUEST_ID, records.size(), records);
-    handler.handle(message, session);
+    handler.handleResponseMessage(message, session);
 
     verifyNoInteractions(nodeTable);
   }
@@ -147,16 +114,11 @@ class NodesHandlerTest {
   public void shouldRejectReceivedRecordsThatAreNotAtCorrectDistance() {
     final NodeInfo nodeInfo = TestUtil.generateNode(9000);
     final int distance = Functions.logDistance(PEER_ID, nodeInfo.getNodeRecord().getNodeId());
-    Request<Void> request =
-        new Request<>(
-            new CompletableFuture<>(),
-            id -> new FindNodeMessage(id, singletonList(distance - 1)),
-            new FindNodeResponseHandler(singletonList(distance)));
-    final RequestInfo requestInfo = RequestInfo.create(REQUEST_ID, request);
-    when(session.getRequestInfo(REQUEST_ID)).thenReturn(Optional.of(requestInfo));
+    final FindNodeResponseHandler handler =
+        new FindNodeResponseHandler(singletonList(distance + 1));
     final List<NodeRecord> records = singletonList(nodeInfo.getNodeRecord());
     final NodesMessage message = new NodesMessage(REQUEST_ID, records.size(), records);
-    handler.handle(message, session);
+    handler.handleResponseMessage(message, session);
 
     verifyNoInteractions(nodeTable);
   }
