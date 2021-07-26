f790ab97e8b81e838728e8e5d97f17c6ad1e4364
Adrian Sutton <adrian.sutton@consensys.net>
2021-07-26 13:15:14 +1000

    Avoid async

--- src/main/java/org/ethereum/beacon/discovery/task/RecursiveLookupTask.java
+++ src/main/java/org/ethereum/beacon/discovery/task/RecursiveLookupTask.java
@@ -10,10 +10,12 @@ import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -73,17 +75,24 @@ public class RecursiveLookupTask {
       return;
     }
     final int maxNodesToQuery = Math.min(availableQuerySlots, remainingTotalQueryLimit);
+
     final Stream<NodeRecord> closestNodesFromBuckets =
         buckets
             .streamClosestNodes(targetNodeId)
             .filter(record -> !queriedNodeIds.contains(record.getNodeId()))
             .limit(maxNodesToQuery);
-    Stream.concat(
-            closestNodesFromBuckets,
-            foundNodes.stream().filter(record -> !queriedNodeIds.contains(record.getNodeId())))
-        .sorted(distanceComparator)
-        .limit(maxNodesToQuery)
-        .forEach(this::queryPeer);
+
+    final Stream<NodeRecord> foundNodesToQuery =
+        foundNodes.stream()
+            .filter(record -> !queriedNodeIds.contains(record.getNodeId()))
+            .limit(maxNodesToQuery);
+
+    // Mix the two sources together and select the closest from either.
+    queryPeers(
+        Stream.concat(closestNodesFromBuckets, foundNodesToQuery)
+            .sorted(distanceComparator)
+            .limit(maxNodesToQuery)
+            .collect(Collectors.toList()));
     if (availableQuerySlots == MAX_CONCURRENT_QUERIES) {
       // There are no in-progress queries even after we looked for more to send so must have run out
       // of possible nodes to query or reached the query limit.
@@ -91,26 +100,31 @@ public class RecursiveLookupTask {
     }
   }
 
-  private void queryPeer(final NodeRecord peer) {
-    queriedNodeIds.add(peer.getNodeId());
-    availableQuerySlots--;
-    remainingTotalQueryLimit--;
-    sendFindNodesRequest
-        .findNodes(peer, Functions.logDistance(peer.getNodeId(), targetNodeId))
-        // Complete async to ensure all queries are sent before any responses are handled
-        // Otherwise we can wind up sending too many queries
-        .whenCompleteAsync(this::onQueryComplete);
+  private void queryPeers(final List<NodeRecord> nodesToQuery) {
+    // Update state to indicate all nodes queried before we start sending requests.
+    // Otherwise if any request completes synchronously we recurse back into this method and wind up
+    // sending too many requests
+    nodesToQuery.stream().map(NodeRecord::getNodeId).forEach(queriedNodeIds::add);
+    availableQuerySlots -= nodesToQuery.size();
+    remainingTotalQueryLimit -= nodesToQuery.size();
+    nodesToQuery.forEach(this::queryPeer);
   }
 
-  private synchronized void onQueryComplete(
-      final Collection<NodeRecord> nodes, final Throwable error) {
-    availableQuerySlots++;
-    if (error != null) {
-      LOG.debug("Find nodes query failed", error);
-    } else {
-      foundNodes.addAll(nodes);
-    }
-    sendRequests();
+  private void queryPeer(final NodeRecord peer) {
+    sendFindNodesRequest
+        .findNodes(peer, Functions.logDistance(peer.getNodeId(), targetNodeId))
+        .whenComplete(
+            (nodes, error) -> {
+              synchronized (this) {
+                availableQuerySlots++;
+                if (error != null) {
+                  LOG.debug("Failed to query node {}", peer.getNodeId(), error);
+                } else {
+                  foundNodes.addAll(nodes);
+                }
+                sendRequests();
+              }
+            });
   }
 
   public interface FindNodesAction {
