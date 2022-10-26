a1899faa97c4669a94c192896024d5767529404d
Justin Traglia <jtraglia@pm.me>
2022-10-26 13:58:17 -0500

    Simplify logDistance function

--- src/main/java/org/ethereum/beacon/discovery/util/Functions.java
+++ src/main/java/org/ethereum/beacon/discovery/util/Functions.java
@@ -238,17 +238,7 @@ public class Functions {
    */
   public static int logDistance(final Bytes nodeId1, final Bytes nodeId2) {
     Bytes distance = nodeId1.xor(nodeId2);
-    int logDistance = Byte.SIZE * distance.size(); // 256
-    final int maxLogDistance = logDistance;
-    for (int i = 0; i < maxLogDistance; ++i) {
-      boolean highBit = ((distance.get(i / 8) >> (7 - (i % 8))) & 1) == 1;
-      if (highBit) {
-        break;
-      } else {
-        logDistance--;
-      }
-    }
-    return logDistance;
+    return 256 - distance.numberOfLeadingZeros();
   }
 
   public static BigInteger distance(final Bytes nodeId1, final Bytes nodeId2) {
